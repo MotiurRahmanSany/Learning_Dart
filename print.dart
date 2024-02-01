99f5bb51bcdfb6b2bad5bd7f4b7b44b47eb699fc
Motiur Rahman <mdmotiurrahmansany@gmail.com>
2024-02-01 10:30:03 +0600

    added files

---
+++ print.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('Hello World');
+  print(3 + 3);
+  print(3 * 4);
+  print('ab' * 10); // will be printed 10 times
+  print('as' + 'asdf');
+  print('24' + '23');
+  print(4 % 3);
+
+}
